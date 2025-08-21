db7d42884a69b41c5067d765b885dcb83eb986ba
“Arielle <arielle.smith@stonybrook.edu>
2025-08-21 11:32:12 -0500

    Most up-to-date bootstrapping function

--- 0_define_functions.R
+++ 0_define_functions.R
@@ -1,17 +1,23 @@
-# Define an adaptation of "yhat" package's function "boot.yhat()" to use "prec = 6"
-# in "calc.yhat()" instead of the "calc.yhat()" default of "prec = 3"
-# - This is so that when running "booteval.yhat(prec = 6)" the output will actually 
-#   show results rounded to 6 decimal places
+# Define an adaptation of "yhat" package's function "boot.yhat()" to optionally
+# take a "prec" argument that can be passed to "calc.yhat()"
+# - This is so that when running "booteval.yhat(prec = D)" the output will actually 
+#   show results rounded to the D decimal places that the user specifies
 # - This is achieved by running "booteval.yhat()" on the results from "boot(statistic 
-#   = boot.yhat.prec6)" instead of on the results from "boot(statistic = boot.yhat)"
+#   = boot.yhat.prec, prec = D)" instead of from "boot(statistic = boot.yhat)"
 # - Link to original "boot.yhat()":
 #   https://github.com/cran/yhat/blob/3beef32b280f86b63c3a7cc4ad08d08a4ad3d4a7/R/boot.yhat.r
 
-boot.yhat.prec6 <- function (data, indices, lmOut, regrout0, prec = 6) 
+boot.yhat.prec <- function (data, indices, lmOut, regrout0, prec = NULL)
 {
   data <- data[indices, ]
   blmOut <- lm(formula = lmOut$call$formula, data = data)
-  regrout <- calc.yhat(blmOut, prec)
+  
+  if (!is.null(prec)) {
+    regrout <- calc.yhat(blmOut, prec)
+  } else {
+    regrout <- calc.yhat(blmOut)
+  }
+  
   pm <- as.vector(regrout$PredictorMetrics[-nrow(regrout$PredictorMetrics), 
   ])
   apsm <- as.vector(as.matrix(regrout$APSRelatedMetrics[-nrow(regrout$APSRelatedMetrics), 
